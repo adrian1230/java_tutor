e9c5b86c3c3c93e7844fed40fa65bf67c0706e2f
adrian1230 <ichbingesenet@gmail.com>
2021-05-29 16:00:07 +0800

    adrian

--- src/com/curnox/Main.java
+++ src/com/curnox/Main.java
@@ -1,10 +1,14 @@
 package com.curnox;
 
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args) {
         int num_1 = 101;
         double num_2 = 101.0;
         String str_1 = "Curnox";
+        Date date_1 = new Date();
         System.out.println("Stop me");
+        System.out.println(date_1);
     }
 }
