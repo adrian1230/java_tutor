86ee535abbb0fbef5e81f9a35bbbe715e0e26d58
adrian1230 <ichbingesenet@gmail.com>
2021-05-29 15:57:47 +0800

    adrian

--- src/com/curnox/Main.java
+++ src/com/curnox/Main.java
@@ -1,9 +1,10 @@
 package com.curnox;
 
 public class Main {
-
     public static void main(String[] args) {
         int num_1 = 101;
+        double num_2 = 101.0;
+        String str_1 = "Curnox";
         System.out.println("Stop me");
     }
 }
