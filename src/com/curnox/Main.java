132016cebd8ba3f7ff74a80844abdda5c8afb3a7
adrian1230 <ichbingesenet@gmail.com>
2021-05-31 01:49:03 +0800

    adrian

--- src/com/curnox/Main.java
+++ src/com/curnox/Main.java
@@ -4,6 +4,15 @@ import java.util.Date;
 
 public class Main {
     public static void main(String[] args) {
+        byte byte_1 = -1;
+        // byte: -128 to 127
+        // short: -32768 to 32767
+        // int: -2147483648 to 2147483647
+        // long: -9223372036854775808 to 9223372036854775807
+        // float: 5 to 7 decimal digits
+        // double: 15 decimal digits
+        // boolean: true or false
+        // char: a single letter or ASCII values
         int num_1 = 101;
         double num_2 = 101.0;
         String str_1 = "Curnox";
